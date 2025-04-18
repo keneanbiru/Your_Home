3d58c226553cf6b42d5e33add4e9de3116c4c5f5
kenean biru <keneanbirubalcha@gmail.com>
2025-04-18 00:25:36 -0800

    listing page update

--- YourHome/src/app/data/property.js
+++ YourHome/src/app/data/property.js
@@ -1,42 +1,41 @@
-
 export const propertyList=[
     {
-    propertyImage:'../../../assets/images/property-1.jpg',
+    propertyImage:'/assets/images/property-1.jpg',
     propertyName:'CMC, Addis Ababa, ETH',
     propertyDetail:'',
     price:'5000',
     location:'Bole'
 },
   {
-    propertyImage:'../../../assets/images/property-2.jpg',
+    propertyImage:'/assets/images/property-2.jpg',
     propertyName:'Addisu Gebeya, Addis Ababa, ETH',
     propertyDetail:'',
     Price:'10000',
     location:'Gulele'
 },
   {
-    propertyImage:'../../../assets/images/property-3.jpg',
+    propertyImage:'/assets/images/property-3.jpg',
     propertyName:'Bethel, Addis Ababa, ETH',
     propertyDetail:'',
     price:'5000',
     location:'Kolfe'
 },
   {
-    propertyImage:'../../../assets/images/property-4.jpg',
+    propertyImage:'/assets/images/property-4.jpg',
     propertyName:'Lebu, Addis Ababa, ETH',
     propertyDetail:'',
     price:'5000',
     location:'Lafto'
 },
   {
-    propertyImage:'../../../assets/images/property-5.jpg',
+    propertyImage:'/assets/images/property-5.jpg',
     propertyName:'Torhayloch, Addis Ababa, ETH',
     propertyDetail:'',
     price:'5000',
     location:'Kolfe'
 },
   {
-    propertyImage:'../../../assets/images/property-6.jpg',
+    propertyImage:'/assets/images/property-6.jpg',
     propertyName:'Kaliti, Addis Ababa, ETH',
     propertyDetail:'',
     price:'5000',
--- YourHome/src/app/js/home_page.js
+++ YourHome/src/app/js/home_page.js
@@ -2,91 +2,88 @@
 
 import { propertyList } from "../data/property.js"
 
-const propertyListComponent=(imageUrl,name,rootName)=>{
-
-const cardContainer=document.createElement('div')
-const list=document.createElement('div')
-const divider=document.createElement('div')
-const dividerBottom=document.createElement('div')
-const propertyBottom=document.createElement('div')
-const image=document.createElement('img')
-const typography=document.createElement('p')
-const propertyDetail=document.createElement('a')
-const price=document.createElement('span')
-const ratingTitle=document.createElement('span')
-const ratingStar1=document.createElement('i')
-const ratingStar2=document.createElement('i')
-const ratingStar3=document.createElement('i')
-const ratingStar4=document.createElement('i')
-const ratingStar5=document.createElement('i')
-const cub=document.createElement('i')
-const bed=document.createElement('i')
-const cubText=document.createElement('p')
-const bedText=document.createElement('p')
-const ratingStarList=document.createElement('span')
+const propertyListComponent=(property)=>{
+    const cardContainer=document.createElement('div')
+    const list=document.createElement('div')
+    const divider=document.createElement('div')
+    const dividerBottom=document.createElement('div')
+    const propertyBottom=document.createElement('div')
+    const image=document.createElement('img')
+    const typography=document.createElement('p')
+    const propertyDetail=document.createElement('a')
+    const price=document.createElement('span')
+    const ratingTitle=document.createElement('span')
+    const ratingStar1=document.createElement('i')
+    const ratingStar2=document.createElement('i')
+    const ratingStar3=document.createElement('i')
+    const ratingStar4=document.createElement('i')
+    const ratingStar5=document.createElement('i')
+    const cub=document.createElement('i')
+    const bed=document.createElement('i')
+    const cubText=document.createElement('p')
+    const bedText=document.createElement('p')
+    const ratingStarList=document.createElement('span')
     
-propertyBottom.classList.add('card-meta')
+    propertyBottom.classList.add('card-meta')
 
-price.innerText=`Price\n 5000 ETB`
-price.style.lineHeight='30px'
+    price.innerText=`Price\n ${property.price || property.Price} ETB`
+    price.style.lineHeight='30px'
 
-ratingTitle.innerText=`Rating`
-ratingStar1.className='fa fa-star'
-ratingStar2.className='fa fa-star'
-ratingStar3.className='fa fa-star'
-ratingStar4.className='fa fa-star'
-ratingStar5.className='fa fa-star'
+    ratingTitle.innerText=`Rating`
+    ratingStar1.className='fa fa-star'
+    ratingStar2.className='fa fa-star'
+    ratingStar3.className='fa fa-star'
+    ratingStar4.className='fa fa-star'
+    ratingStar5.className='fa fa-star'
 
-ratingStar1.style.color='orange'
-ratingStar2.style.color='orange'
-ratingStar3.style.color='orange'
-ratingStar4.style.color='orange'
-ratingStar5.style.color='orange'
+    ratingStar1.style.color='orange'
+    ratingStar2.style.color='orange'
+    ratingStar3.style.color='orange'
+    ratingStar4.style.color='orange'
+    ratingStar5.style.color='orange'
 
+    ratingStarList.appendChild(ratingTitle)
+    ratingStarList.appendChild(ratingStar1)
+    ratingStarList.appendChild(ratingStar2)
+    ratingStarList.appendChild(ratingStar3)
+    ratingStarList.appendChild(ratingStar4)
+    ratingStarList.appendChild(ratingStar5)
 
-ratingStarList.appendChild(ratingTitle)
-ratingStarList.appendChild(ratingStar1)
-ratingStarList.appendChild(ratingStar2)
-ratingStarList.appendChild(ratingStar3)
-ratingStarList.appendChild(ratingStar4)
-ratingStarList.appendChild(ratingStar5)
+    cub.className='fa fa-cube'
+    bed.className='fa fa-bed'
+    list.classList.add('property-info')
+    cubText.innerText='800ftx200ft'
+    bedText.innerText='Bed'
+    list.appendChild(cub)
+    list.appendChild(cubText)
+    list.appendChild(bed)
+    list.appendChild(bedText)
+    divider.classList.add('divider')
+    dividerBottom.classList.add('divider')
+    cardContainer.classList.add('property-card-container')
+    image.src=property.propertyImage
+    image.classList.add('property-image')
+    typography.innerText=property.propertyName
+    typography.classList.add('property-name')
+    propertyDetail.innerText='Property Detail'
+    propertyDetail.classList.add('property-detail')
+    propertyDetail.href=`/src/app/pages/property_details.html?property=${encodeURIComponent(property.propertyName)}`
 
-cub.className='fa fa-cube'
-bed.className='fa fa-bed'
-list.classList.add('property-info')
-cubText.innerText='800ftx200ft'
-bedText.innerText='Bed'
-list.appendChild(cub)
-list.appendChild(cubText)
-list.appendChild(bed)
-list.appendChild(bedText)
-divider.classList.add('divider')
-dividerBottom.classList.add('divider')
-cardContainer.classList.add('property-card-container')
-image.src=imageUrl
-image.classList.add('property-image')
-typography.innerText=name
-typography.classList.add('property-name')
-propertyDetail.innerText='Property Detail'
-propertyDetail.classList.add('property-detail')
-propertyDetail.href='.src/app/pages/property_details.html'
+    propertyBottom.appendChild(price)
+    propertyBottom.appendChild(ratingStarList)
 
-propertyBottom.appendChild(price)
-propertyBottom.appendChild(ratingStarList)
+    cardContainer.appendChild(image)
+    cardContainer.appendChild(typography)
+    cardContainer.appendChild(propertyDetail)
+    cardContainer.appendChild(divider)
+    cardContainer.appendChild(list)
+    cardContainer.appendChild(dividerBottom)
+    cardContainer.appendChild(propertyBottom)
 
-cardContainer.appendChild(image)
-cardContainer.appendChild(typography)
-cardContainer.appendChild(propertyDetail)
-cardContainer.appendChild(divider)
-cardContainer.appendChild(list)
-cardContainer.appendChild(dividerBottom)
-cardContainer.appendChild(propertyBottom)
-
-document.getElementById(rootName).appendChild(cardContainer)
-
+    document.getElementById('property-lists').appendChild(cardContainer)
 }
 
-propertyList.splice(0,3)?.map((data,index)=>{
-    propertyListComponent(data.propertyImage,data.propertyName,'property-lists')
-    
+// Display first 3 properties without modifying the original array
+propertyList.slice(0,3).forEach((property)=>{
+    propertyListComponent(property)
 })
